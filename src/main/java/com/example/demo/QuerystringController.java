0dc3f3252c010b8b15abe78325f5511dc11fb170
azteckind <73482202+azteckind@users.noreply.github.com>
2020-12-08 12:41:12 -0800

    Worked on query string controller with some success

--- src/main/java/com/example/demo/QuerystringController.java
+++ src/main/java/com/example/demo/QuerystringController.java
@@ -2,19 +2,40 @@ package com.example.demo;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Map;
+
 @RestController
-@RequestMapping("/app")
+//@RequestMapping("/app")
 public class QuerystringController {
 
-    @GetMapping("/foo")
-    public String getFoo() {
-        return "foo";
+    @GetMapping("/individual-example")
+    public String getIndividualParams(@RequestParam String filter) {
+          return String.format("Filter is : %s", filter);
     }
 
-    @GetMapping("/bar")
-    public String getBar() {
-        return "bar";
+    @GetMapping("/people")
+    public String getPeople(
+            @RequestParam("sort-by") String sortBy,
+            @RequestParam(value = "sort-dir") String sortDirection) {
+        return String.format("sortBy is %s and sortDirection is %s", sortBy, sortDirection);
     }
-}//Pre-fixing all paths in a controller
+
+    @GetMapping("/vehicle")
+    public String myCoolMethod(@RequestParam(required = false) String type) {
+        return type;
+    }
+
+    @GetMapping("/other")
+    public String myCoolMethod(@RequestParam(value = "type", defaultValue = "car") String type) {
+        return type;
+    }
+
+    @GetMapping("/map-example")
+    public String getMapParams(@RequestParam Map querystring) {
+        return querystring.toString();
+    }
+
+} //Created Query string separate from the rest
--- src/test/java/com/example/demo/QuerystringControllerTest.java
+++ src/test/java/com/example/demo/QuerystringControllerTest.java
@@ -1,44 +1,24 @@
 package com.example.demo;
 
-
 import org.junit.jupiter.api.Test;
-import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.RequestBuilder;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-//@RunWith(SpringRunner.class)
-@WebMvcTest(HelloController.class)
-public class HelloControllerTest {
+@WebMvcTest(QuerystringController.class)
+public class QuerystringControllerTest {
     @Autowired
     MockMvc mvc;
-
-    @Test
-    public void testingHello() throws Exception {
-        RequestBuilder request = MockMvcRequestBuilders.get("/hello");
-
-        this.mvc.perform(request)
-                .andExpect(status().isOk())
-                .andExpect(content().string("Hello World"));
-    }//end of test
-
-    @Test
-    public void testingPI() throws Exception {
-        this.mvc.perform(get("/math/pi"))
-                .andExpect(status().isOk());
-    }//end of test
 
     @Test
     public void testingIndexEndpoint() throws Exception {
         this.mvc.perform(get("/vehicles?year=1987%doors=2"))
                 .andExpect(status().isOk());
     }//end of test
-
 }
