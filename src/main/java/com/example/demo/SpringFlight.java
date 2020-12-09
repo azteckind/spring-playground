f5ca5ed6578a556d13f0dc3a62ed878d214fb5f6
azteckind <73482202+azteckind@users.noreply.github.com>
2020-12-09 14:31:15 -0800

    Still having issues with the JSON path for $.Departs and $.Ticket

--- src/main/java/com/example/demo/SpringFlight.java
+++ src/main/java/com/example/demo/SpringFlight.java
@@ -15,22 +15,60 @@ public class SpringFlight {
     private LocalDateTime departureDateTime;
     private final HashSet<Ticket> tickets = new HashSet<>();
 
-//    @JsonFormat(pattern = "yyyy-MM-dd HH:mm")
-//    public LocalDateTime getDepartureDateTime() {
-//        return departureDateTime;
-//    }
-
     @GetMapping("/flight")
     public String getFlight() {
         return "Flying High as a Kite";
     }
 
-    @JsonGetter("departs")
+    @JsonGetter("Departs")
     public String getDepartureDateTime() {
         return departureDateTime.format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm"));
     }
 
-    private static class Ticket {
-
+    @JsonGetter("Tickets")
+    public HashSet<Ticket> getTickets() {
+        return this.tickets;
     }
+
+    public void setDepartureDateTime(int year, int month, int date, int hour, int minute) {
+        this.departureDateTime = LocalDateTime.of(year, month, date, hour, minute);
+    }
+
+    private static class Ticket {
+        private final int price;
+        private final Person passenger;
+
+        public Ticket(int price, Person passenger) {
+            this.price = price;
+            this.passenger = passenger;
+        }
+
+        public int getPrice() {
+            return price;
+        }
+
+        public Person getPassenger() {
+            return passenger;
+        }
+
+    }//end of Ticket class
+
+    private static class Person {
+        private final String firstName;
+        private final String lastName;
+
+        public Person(String firstName, String lastName) {
+            this.firstName = firstName;
+            this.lastName = lastName;
+        }
+
+        public String getFirstName() {
+            return firstName;
+        }
+
+        public String getLastName() {
+            return lastName;
+        }
+
+    }//end of Person class
 }
--- src/test/java/com/example/demo/SpringFlightTest.java
+++ src/test/java/com/example/demo/SpringFlightTest.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
 
 import static org.hamcrest.Matchers.is;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
@@ -25,8 +26,19 @@ public class SpringFlightTest {
                     .accept(MediaType.APPLICATION_JSON)
                     .contentType(MediaType.APPLICATION_JSON))
                 .andExpect(status().isOk())
-                .andExpect(jsonPath("$.departs", is("2017-04-21 14:34")));
+                .andExpect(jsonPath("$.Departs", is("2017-04-21 14:34")));
 
     }//end of test for departure flight
+
+    @Test
+    public void testingTicketing() throws Exception {
+        this.mvc.perform(get("/flights/flight")
+                    .accept(MediaType.APPLICATION_JSON)
+                    .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.Tickets[0].Passenger.FirstName", is("Some name")))
+                .andExpect(jsonPath("$.Tickets[0].Passenger.LastName", is("Some other name")))
+                .andExpect(jsonPath("$.Tickets[0].Price", is(200)));
+    }//end of ticketing test
 
 }
