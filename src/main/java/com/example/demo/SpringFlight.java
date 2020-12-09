79f7759ec4e65731cc343b8e74f53f20fe48adca
azteckind <73482202+azteckind@users.noreply.github.com>
2020-12-09 14:02:02 -0800

    Working on Endpoint 1 of Spring Flight

--- src/main/java/com/example/demo/SpringFlight.java
+++ src/main/java/com/example/demo/SpringFlight.java
@@ -1,4 +1,36 @@
 package com.example.demo;
 
+import com.fasterxml.jackson.annotation.JsonGetter;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.HashSet;
+
+@RestController
+@RequestMapping("/flights")
 public class SpringFlight {
+    private LocalDateTime departureDateTime;
+    private final HashSet<Ticket> tickets = new HashSet<>();
+
+//    @JsonFormat(pattern = "yyyy-MM-dd HH:mm")
+//    public LocalDateTime getDepartureDateTime() {
+//        return departureDateTime;
+//    }
+
+    @GetMapping("/flight")
+    public String getFlight() {
+        return "Flying High as a Kite";
+    }
+
+    @JsonGetter("departs")
+    public String getDepartureDateTime() {
+        return departureDateTime.format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm"));
+    }
+
+    private static class Ticket {
+
+    }
 }
---
+++ src/test/java/com/example/demo/SpringFlightTest.java
@@ -0,0 +1,32 @@
+package com.example.demo;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.hamcrest.Matchers.is;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+
+@WebMvcTest(SpringFlight.class)
+public class SpringFlightTest {
+    @Autowired
+    private MockMvc mvc;
+    ObjectMapper objectMapper = new ObjectMapper();
+
+    @Test
+    public void testingDepartureFlight() throws Exception {
+        this.mvc.perform(get("/flights/flight")
+                    .accept(MediaType.APPLICATION_JSON)
+                    .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.departs", is("2017-04-21 14:34")));
+
+    }//end of test for departure flight
+
+}
