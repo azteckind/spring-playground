e41007e9a378fd69bf8f78ad0f1c813b36ce4496
azteckind <73482202+azteckind@users.noreply.github.com>
2020-12-11 12:43:33 -0800

    Finally! Got the delete function to work!

--- src/main/java/com/example/demo/LessonsController.java
+++ src/main/java/com/example/demo/LessonsController.java
@@ -2,6 +2,8 @@ package com.example.demo;
 
 import org.springframework.web.bind.annotation.*;
 
+import java.util.Optional;
+
 @RestController
 @RequestMapping("/lessons")
 public class LessonsController {
@@ -12,6 +14,17 @@ public class LessonsController {
         this.repository = repository;
     }
 
+    @GetMapping("/{id}")
+    public Optional<Lesson> show(@PathVariable int id) {
+        Long newId = (long) id;
+        return this.repository.findById(newId);
+    }
+
+    @DeleteMapping("/{id}")
+    public void delete(@PathVariable Long id) {
+        this.repository.deleteById(id);
+    }
+
     @GetMapping("")
     public Iterable<Lesson> all() {
         return this.repository.findAll();
@@ -19,6 +32,13 @@ public class LessonsController {
 
     @PostMapping("")
     public Lesson create(@RequestBody Lesson lesson) {
+        return this.repository.save(lesson);
+    }
+
+    @PatchMapping("/{id}")
+    public Lesson patch(@PathVariable Long id, @RequestBody Lesson lesson) {
+        Lesson oldLesson = this.repository.findById(id).get();
+        lesson.setId(oldLesson.getId());
         return this.repository.save(lesson);
     }
 
